ef1139b8f281726df7de1b7db1163daa20daa8c8
Embla <emblajonsson@gmail.com>
2019-12-22 19:37:53 +0100

    removed the ellipse from the startmenu

--- sketch/splashScreen.ts
+++ sketch/splashScreen.ts
@@ -17,11 +17,12 @@ class SplashScreen {
     fill("black");
     rect(this.x, this.y, this.w, this.h);
     fill("white");
-    ellipse(width / 2, height / 2, 400, 400);
     textAlign(CENTER);
-    fill("black");
-    text("The game CIGGEN", width / 2, height / 3);
-    text("click anywhere to start", width / 2, height / 2);
+    textSize(25)
+    text("CIGGEN", width / 2, height / 3);
+    textSize(14)
+    text("click anywhere to start", width / 2, height / 2.5);
+   textSize(12)
     text(
       "Ett puzzelspel blandat med plattformsspel. \n Du skall bygga en bana för att hjälpa den stackars Jonathan \n  till sin efterlängtade ciggarett. \n Du kan när du vill pausa och gå in i byggfasen och börja om. \n Akta dig för grönsakerna på botten, \n Jonathan avskyr dem och du vet inte vad som kan hända..",
       width / 2,
