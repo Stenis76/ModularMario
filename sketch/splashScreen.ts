243a45d43b335b954b09c54a537e61467c9b8344
Embla <emblajonsson@gmail.com>
2019-12-28 16:47:52 +0100

    small changes in splashScreen

--- sketch/splashScreen.ts
+++ sketch/splashScreen.ts
@@ -1,5 +1,5 @@
 let currentScreen: number = 0;
-let infoText = "Ett puzzelspel blandat med plattformsspel. \n Du skall bygga en bana för att hjälpa den stackars Jonathan \n  till sin efterlängtade ciggarett. \n Du kan när du vill pausa och gå in i byggfasen och börja om. \n Akta dig för grönsakerna på botten, \n Jonathan avskyr dem och du vet inte vad som kan hända..";
+let infoText: string = "Ett puzzelspel blandat med plattformsspel. \n Du skall bygga en bana för att hjälpa den stackars Jonathan \n  till sin efterlängtade ciggarett. \n Du kan när du vill pausa och gå in i byggfasen och börja om. \n Akta dig för grönsakerna på botten, \n Jonathan avskyr dem och du vet inte vad som kan hända..";
 
 class SplashScreen {
   w: number;
@@ -7,7 +7,7 @@ class SplashScreen {
   x: number;
   y: number;
 
-  constructor(w: number, h: number, x: number, y: number) {
+  private constructor(w: number, h: number, x: number, y: number) {
     this.w = w;
     this.h = h;
     this.x = x;
@@ -23,7 +23,7 @@ class SplashScreen {
     text("CIGGEN", width / 2, height / 3);
     textSize(14)
     text("click anywhere to start", width / 2, height / 2.5);
-   textSize(12)
+    textSize(12)
     text(infoText, width / 2, height / 2 + 20);
     // console.log(this.h, this.w);
   }
